cbf55c69966c1106378110bc6c3fc138c43fb98a
Santhosh-N <59107079+Santhosh-N@users.noreply.github.com>
2020-12-23 08:22:27 +0000

    Add files via upload

---
+++ SlowMotion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+
+    public float slowTime = 0.2f;
+    private bool isSlowed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (isSlowed)
+            {
+                isSlowed = false;
+                Time.timeScale = 1;
+                Time.fixedDeltaTime = Time.deltaTime;
+            }
+            else
+            {
+                isSlowed = true;
+                Time.timeScale = slowTime;
+                Time.fixedDeltaTime = slowTime * Time.deltaTime;
+            }
+        }
+    }
+}
